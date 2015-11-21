6ada68d8384a4310070b142db11421848b233d84
Appespi <anappe_@hotmail.com>
2015-11-21 17:53:38 +0000

    crazyAsteroid done

--- src/spacerace/asteroids/CrazyAsteroid.java
+++ src/spacerace/asteroids/CrazyAsteroid.java
@@ -1,7 +1,21 @@
 package spacerace.asteroids;
 
+import spacerace.Asteroid;
+import spacerace.Coord2D;
 
-// TODO 
-public final class CrazyAsteroid {
-
+public final class CrazyAsteroid extends Asteroid{
+	private int passos = 0;
+	public CrazyAsteroid(Coord2D location, double direction, int speed) {
+		super(location, direction, speed);
+		
+	}
+	public void step(){
+		if (passos == 100){
+			this.setDirection(Math.random());
+			passos = 0;
+		}
+		else{
+			passos = passos + 1;
+		}
+	}
 }
