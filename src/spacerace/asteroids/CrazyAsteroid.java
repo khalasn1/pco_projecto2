a242b80946c87b05a773979f663cc8a4d8aa9119
sherby <sherby@bitbucket.org>
2015-11-21 18:11:53 +0000

    CrazyPlayer

--- src/spacerace/asteroids/CrazyAsteroid.java
+++ src/spacerace/asteroids/CrazyAsteroid.java
@@ -4,8 +4,10 @@ import spacerace.Asteroid;
 import spacerace.Coord2D;
 
 public final class CrazyAsteroid extends Asteroid{
-	private int passos = 0;
-	public CrazyAsteroid(Coord2D location, double direction, int speed) {
+
+    private int passos = 0;
+
+    public CrazyAsteroid(Coord2D location, double direction, int speed) {
 		super(location, direction, speed);
 		
 	}
--- src/spacerace/players/CrazyPlayer.java
+++ src/spacerace/players/CrazyPlayer.java
@@ -4,16 +4,23 @@ import spacerace.AIPlayer;
 import spacerace.Coord2D;
 import spacerace.GameState;
 
-// TODO
-public final class CrazyPlayer extends AIPlayer { 
-  
-  public CrazyPlayer(Coord2D location, double direction, int referenceSpeed) {
-    super(location, direction, referenceSpeed);
-  }
-  
-  @Override
-  public void step(GameState gs) {
-    // TODO
-  }
+
+public final class CrazyPlayer extends AIPlayer {
+
+    private int passos = 0;
+
+    public CrazyPlayer(Coord2D location, double direction, int referenceSpeed) {
+        super(location, direction, referenceSpeed);
+    }
+
+    @Override
+    public void step(GameState gs) {
+        if (passos == 50) {
+            this.setDirection(Math.random());
+            passos = 0;
+        } else {
+            passos = passos + 1;
+        }
+    }
 }
 
