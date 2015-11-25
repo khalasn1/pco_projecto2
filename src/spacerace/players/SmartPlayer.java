9b0bbf951ec49e9e55c62f8fc26347b0df63b4f4
sherby <sherby@bitbucket.org>
2015-11-25 17:19:28 +0000

    SmartPlayer finally moving

--- src/spacerace/players/SmartPlayer.java
+++ src/spacerace/players/SmartPlayer.java
@@ -6,41 +6,73 @@ import spacerace.areas.*;
 // TODO
 public class SmartPlayer extends AIPlayer {
 
-  public SmartPlayer(Coord2D location, double direction, int referenceSpeed) {
-    super(location, direction, referenceSpeed);
-  }
+    /* Construtor SmartPlayer
+     */
 
-  public void step (GameState gs) {
 
-      //  d = 0,90,180,270;
+    public SmartPlayer(Coord2D location, double direction, int referenceSpeed) {
 
-      /*
-      /
-      Area look = gs.getArea(getLocation().displace(Constants.ELEM_WIDTH * 1.1 , getDirection()));
-      /
-      if (look instanceof BlackHole || look instanceof Planet) {
-        // ignore d
-      }
+        super(location, direction, referenceSpeed);
+    }
 
-      if (look instanceof EmptyArea) {
-        /* evaluate d to waypoint destiny
+    public void step (GameState gs) {
 
-          setDirection(0);
+        Coord2D p = Constants.normalize(getLocation());
 
-          Coord2D p = gs.getWayPointLocation(getTargetWayPoint());
+        Area look = gs.getArea(p.displace(Constants.ELEM_WIDTH * 1.1, getDirection()));
 
-          double distance = p.distanceTo(p);
-      }
+        Area lookOne = gs.getArea(p.displace(Constants.ELEM_WIDTH * 1.1, 0));
+        Area lookTwo = gs.getArea(p.displace(Constants.ELEM_WIDTH * 1.1, 90));
+        Area lookThree = gs.getArea(p.displace(Constants.ELEM_WIDTH * 1.1, 180));
+        Area lookFour = gs.getArea(p.displace(Constants.ELEM_WIDTH * 1.1, 270));
 
-      if (look instanceof Dust) {
-        /* evaluate d to waypoint destiny * 1.25
-      }
+        Coord2D nextWaypoint = gs.getWayPointLocation(getTargetWayPoint());
 
-      if (look instanceof WormHole) {
-        /* not sure
-      }
 
-    */
-  }
-  
+        if (nextWaypoint.compareTo(lookFour.getLocation()) > nextWaypoint.compareTo(lookThree.getLocation())) {
+
+            setDirection(180);
+
+        }
+        else if (nextWaypoint.compareTo(lookThree.getLocation()) > nextWaypoint.compareTo(lookTwo.getLocation())) {
+            setDirection(90);
+
+        }
+        else if (nextWaypoint.compareTo(lookTwo.getLocation()) > nextWaypoint.compareTo(lookOne.getLocation())) {
+            setDirection(0);
+
+        }
+        else {
+            setDirection(270);
+        }
+
+
+        //cria a var "look" aqui. porque entretanto, a dir muda
+
+
+        if ( look instanceof EmptyArea) {
+        }
+
+        if (look instanceof BlackHole || look instanceof Planet) {
+            // ignore d
+        }
+
+        if (look instanceof EmptyArea) {
+
+
+            setDirection(0);
+
+            Coord2D b = gs.getWayPointLocation(getTargetWayPoint());
+
+            double distance = p.distanceTo(p);
+        }
+
+        if (look instanceof Dust) {
+
+        }
+
+        if (look instanceof WormHole) {
+
+        }
+    }
 }
