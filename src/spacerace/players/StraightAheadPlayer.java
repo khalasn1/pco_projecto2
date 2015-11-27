fefb23685e93c2327bff0c8dac8a4a214594c24c
sherby <sherby@bitbucket.org>
2015-11-27 16:18:51 +0000

    cenas

--- src/spacerace/players/StraightAheadPlayer.java
+++ src/spacerace/players/StraightAheadPlayer.java
@@ -4,18 +4,34 @@ import spacerace.AIPlayer;
 import spacerace.Coord2D;
 import spacerace.GameState;
 
+/* Class StraightAheadPlayer
+ *
+ * Moves a Player in a Straigh line in direction of the waypoints
+ */
+
 public class StraightAheadPlayer extends AIPlayer {
 
-  public StraightAheadPlayer(Coord2D location, double direction, int referenceSpeed) {
-    super(location, direction, referenceSpeed);
-  }
- 
-  @Override
-  public void step(GameState gs) {
+    /* Construtor
+     *
+     */
 
-      Coord2D p = gs.getWayPointLocation(getTargetWayPoint());
+    public StraightAheadPlayer(Coord2D location, double direction, int referenceSpeed) {
+        super(location, direction, referenceSpeed);
+    }
 
-      setDirection(getLocation().directionTo(p));
-  }
+    /* Method step.
+     *
+     * Overrides the step() from AIPlayer class
+     * @param GameState gs
+     *
+     */
+
+    @Override
+    public void step(GameState gs) {
+
+        Coord2D p = gs.getWayPointLocation(getTargetWayPoint());
+
+        setDirection(getLocation().directionTo(p));
+    }
 
 }
